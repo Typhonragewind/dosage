83198d0152c38d871dab967fef33cd610fe8e8b3
typhon <typhonragewind@gmail.com>
2023-06-12 15:07:33 +0100

    update Vibe

--- dosagelib/plugins/v.py
+++ dosagelib/plugins/v.py
@@ -49,20 +49,20 @@ class VGCats(_ParserScraper):
     help = 'Index format: n (unpadded)'
 
 
-class vibe(ParserScraper):
-    import requests
-    import re
-    base_url = 'http://www.vibecomic.com/vibe/'
-    response = requests.get(base_url)
-    page_text = response.text
-    match = re.search(r'<a\s+href="([^"]+)"\s+class="last"\s+rel="index">', page_text)
-    url = match.group(1)
-    stripUrl = base_url + '%s'
+class Vibe(ParserScraper):
+    starter = indirectStarter
+    url = 'http://www.vibecomic.com/vibe/'
+    stripUrl = url + '%s'
     firstStripUrl = stripUrl % ''
     imageSearch = '//div[@id="cc-comicbody"]//img'
     prevSearch = '//a[@rel="prev"]'
     nextSearch = '//a[@rel="next"]'
+    latestSearch = '//a[@class="last"]'
     help = 'Index format: VIBEnnn (padded)'
+
+    def namer(self, imageUrl, pageUrl):
+        number = imageUrl.rsplit('/')[-1].split('-', 1)[1].rsplit('.')[0]
+        return number
 
 
 class VickiFox(_ParserScraper):
